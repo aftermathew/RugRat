f4dbd380a2f91e0364639f0f86184f71306ec20c
Mathew Chasan <mathew@synapse.com>
2010-06-06 22:04:21 -0700

    q and a navigation basics working

--- iPhone/RRQuestionViewController.m
+++ iPhone/RRQuestionViewController.m
@@ -28,7 +28,6 @@
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"Questions";
     selectedTopic = nil;
 }
 
